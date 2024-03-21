dee112f02a40dbde672e59c1b734d46bc04fe16e
awais512 <raza.awais25@gmail.com>
2024-03-21 02:16:52 +0000

    Marketing page structure

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Button variant="primary">Click ME</Button>
-    </div>
-  );
+  return <div>This is Marketing Page</div>;
 }
