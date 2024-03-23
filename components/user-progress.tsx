6fde77d66e93f168725c9c20141ade395a0f8a30
awais512 <raza.awais25@gmail.com>
2024-03-23 08:56:10 +0000

    User Progress initial layout

---
+++ components/user-progress.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+
+type Props = {
+  activeCourse: { imageSrc: string; title: string };
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+
+const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="/courses">
+        <Button variant="ghost">
+          <Image
+            className="rounded-md border"
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            width={32}
+            height={32}
+          />
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            alt="Points"
+            height={28}
+            width={28}
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="/heart.svg"
+            alt="Heart"
+            height={22}
+            width={22}
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="w-4 h-4 stroke-[3]" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default UserProgress;
