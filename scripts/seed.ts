ef076364492069fa9af2ca7a5b4c2c70e2578b61
awais512 <raza.awais25@gmail.com>
2024-03-26 11:26:23 +0000

    Extend the seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,12 @@ const main = async () => {
     console.log("Seeding database");
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+
     await db.insert(schema.courses).values([
       {
         id: 1,
@@ -34,6 +40,31 @@ const main = async () => {
         id: 4,
         title: "Croatian",
         imageSrc: "/hr.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of spanish",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "Nouns",
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: "Verbs",
       },
     ]);
     console.log("Seeding finished");
