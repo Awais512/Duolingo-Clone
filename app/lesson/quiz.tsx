124cb17a393ec2b77ba3ea7b81f4d286005cde75
awais512 <raza.awais25@gmail.com>
2024-03-29 01:33:21 +0000

    Lesson Page

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { challengeOptions, challenges } from "@/db/schema";
+import { useState } from "react";
+import Header from "./header";
+
+type Props = {
+  lessonId: number;
+  initialHearts: number;
+  initialPercentage: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  userSubscription: any;
+};
+
+export const Quiz = ({
+  lessonId,
+  initialHearts,
+  initialPercentage,
+  initialLessonChallenges,
+  userSubscription,
+}: Props) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
+
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};
