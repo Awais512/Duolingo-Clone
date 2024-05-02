0facdef6fb0a3c4db8c8c794f1bc1ef53714bb41
awais512 <raza.awais25@gmail.com>
2024-05-02 13:14:17 +0100

    Create Course

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -3,12 +3,20 @@ import simpleRestProvider from "ra-data-simple-rest";
 
 import { Admin, Resource } from "react-admin";
 import { CourseList } from "./course/list";
+import { CourseCreate } from "./course/create";
+import { CourseEdit } from "./course/edit";
 
 const dataProvider = simpleRestProvider("/api");
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="courses" recordRepresentation="title" list={CourseList} />
+      <Resource
+        name="courses"
+        recordRepresentation="title"
+        list={CourseList}
+        create={CourseCreate}
+        edit={CourseEdit}
+      />
     </Admin>
   );
 };
---
+++ app/admin/course/edit.tsx
@@ -0,0 +1,13 @@
+import { Edit, TextInput, SimpleForm, required } from "react-admin";
+
+export const CourseEdit = () => {
+  return (
+    <Edit>
+      <SimpleForm>
+        <TextInput source="id" validate={[required()]} label="ID" />
+        <TextInput source="title" validate={[required()]} label="Title" />
+        <TextInput source="imageSrc" validate={[required()]} label="Image" />
+      </SimpleForm>
+    </Edit>
+  );
+};
