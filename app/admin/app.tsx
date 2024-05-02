8ae0b0485ee4c689e754aa86ddd0074c8eacbb0d
awais512 <raza.awais25@gmail.com>
2024-05-02 17:33:21 +0100

    Create Units with admin panel

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -5,6 +5,9 @@ import { Admin, Resource } from "react-admin";
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
 import { CourseEdit } from "./course/edit";
+import { UnitList } from "./unit/list";
+import { UnitCreate } from "./unit/create";
+import { UnitEdit } from "./unit/edit";
 
 const dataProvider = simpleRestProvider("/api");
 const App = () => {
@@ -16,6 +19,13 @@ const App = () => {
         list={CourseList}
         create={CourseCreate}
         edit={CourseEdit}
+      />
+      <Resource
+        name="units"
+        recordRepresentation="title"
+        list={UnitList}
+        create={UnitCreate}
+        edit={UnitEdit}
       />
     </Admin>
   );
